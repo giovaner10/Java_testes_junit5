0b25895fc53be0b76b71fd1fd2410d2c2b63c9ea
giovaner10 <giovanerr10@gmail.com>
2022-05-06 00:09:06 -0300

    assercoes eadicao no pom para compilar

--- TestesUnitarios1/src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ TestesUnitarios1/src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -7,8 +7,7 @@ import java.util.Date;
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
-import org.junit.Assert;
-import org.junit.Test;
+import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoService {
 	
@@ -29,20 +28,19 @@ public class LocacaoService {
 		
 		return locacao;
 	}
-
-	@Test
-	public void teste(){
-
+	
+	public static void main(String[] args) {
 		//cenario
 		LocacaoService service = new LocacaoService();
-
-		//acao
-		Locacao locacao = service.alugarFilme(new Usuario(), new Filme());
-
-		//verificacao
-		System.out.println(locacao.getDataLocacao());
-		Assert.assertTrue(locacao.getDataLocacao() != null);
-
+		Usuario usuario = new Usuario("Usuario 1");
+		Filme filme = new Filme("Filme 1", 2, 5.0);
 		
+		//acao
+		Locacao locacao = service.alugarFilme(usuario, filme);
+		
+		//verificacao
+		System.out.println(locacao.getValor() == 5.0);
+		System.out.println(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+		System.out.println(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
 	}
 }
