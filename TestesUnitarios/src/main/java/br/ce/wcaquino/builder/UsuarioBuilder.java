492590e5d2bc8655c6da2b4c94d14061e83cfa35
giovaner10 <giovanerr10@gmail.com>
2022-05-08 10:40:58 -0300

    builder sem lombok

---
+++ TestesUnitarios/src/main/java/br/ce/wcaquino/builder/UsuarioBuilder.java
@@ -0,0 +1,21 @@
+package br.ce.wcaquino.builder;
+
+import br.ce.wcaquino.entidades.Usuario;
+
+public class UsuarioBuilder {
+
+    private Usuario usuario;
+
+    private static UsuarioBuilder usuarioBuilder() {
+        UsuarioBuilder usuarioBuilder = new UsuarioBuilder();
+        usuarioBuilder.usuario = new Usuario();
+
+        return usuarioBuilder;
+    }
+
+    private static Usuario usuario() {
+
+        return usuario();
+    }
+
+}
