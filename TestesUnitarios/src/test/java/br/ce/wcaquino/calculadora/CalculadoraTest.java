35e52936cf875d725726ee59d44fb3aa700c48c0
giovaner10 <giovanerr10@gmail.com>
2022-05-08 09:28:21 -0300

    @ignore do junit

--- TestesUnitarios/src/test/java/br/ce/wcaquino/calculadora/CalculadoraTest.java
+++ TestesUnitarios/src/test/java/br/ce/wcaquino/calculadora/CalculadoraTest.java
@@ -1,11 +1,19 @@
 package br.ce.wcaquino.calculadora;
 
+import br.ce.wcaquino.exceptions.NaoPodeDividirPorZeroException;
+import org.assertj.core.api.Assertions;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
+
 import static org.assertj.core.api.Assertions.*;
 
 
 public class CalculadoraTest {
+    public ExpectedException expectedException = ExpectedException.none();
+
+
     @Test
     public void somaDeDoisValores(){
         int a = 5;
@@ -19,6 +27,7 @@ public class CalculadoraTest {
     }
 
     @Test
+    @Ignore
     public void subtrairDoisValores(){
         int a = 5;
         int b = 6;
@@ -39,6 +48,21 @@ public class CalculadoraTest {
 
         assertThat(soma).isEqualTo(a/b);
         Assert.assertEquals(a/b, soma);
+
+    }
+
+    @Test
+    public void dividirDoisValoresBaseZeroException() throws NaoPodeDividirPorZeroException{
+        int a = 4;
+        int b = 0;
+
+        //expectedException.expect(NaoPodeDividirPorZeroException.class);
+
+
+        //assertj e junit5
+        Assertions.assertThatThrownBy(()->Calculadora.dividir(a, b)).isInstanceOf(NaoPodeDividirPorZeroException.class);
+        org.junit.jupiter.api.Assertions.assertThrows(NaoPodeDividirPorZeroException.class,()-> Calculadora.dividir(a,b));
+
 
     }
 }
