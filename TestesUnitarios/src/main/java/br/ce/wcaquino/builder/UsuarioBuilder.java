cd711cd0cf65ae5b5a88c3fd61dd6edde9649daa
giovaner10 <giovanerr10@gmail.com>
2022-05-08 14:50:41 -0300

    arquivos ts para teste

--- TestesUnitarios/src/main/java/br/ce/wcaquino/builder/UsuarioBuilder.java
+++ TestesUnitarios/src/main/java/br/ce/wcaquino/builder/UsuarioBuilder.java
@@ -18,4 +18,6 @@ public class UsuarioBuilder {
         return usuario();
     }
 
+
+
 }
---
+++ TestesUnitarios/src/main/java/br/ce/wcaquino/calculadora/rxjs.js
@@ -0,0 +1,11 @@
+const {Observable} = require("rxjs")
+
+const obs = Observable.create(subscriber =>
+{
+    subscriber.next("rxjs")
+    subscriber.complete()
+})
+
+obs.subscribe(txt =>{
+    console.log(txt)
+})
