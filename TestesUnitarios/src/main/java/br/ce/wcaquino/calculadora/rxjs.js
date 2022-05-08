cb105daafb4dae9500fd01cc9399db25825bb0b4
giovaner10 <giovanerr10@gmail.com>
2022-05-08 16:36:39 -0300

    commit

--- TestesUnitarios/src/main/java/br/ce/wcaquino/calculadora/rxjs.js
+++ TestesUnitarios/src/main/java/br/ce/wcaquino/calculadora/rxjs.js
@@ -5,7 +5,8 @@ const obs = Observable.create(subscriber =>
     subscriber.next("rxjs")
     subscriber.complete()
 })
-
+//revendo os conceitos semanticos
+wwwwwwww
 obs.subscribe(txt =>{
     console.log(txt)
 })
